cdef1caf1ecc74437013fe2c13f1cd11563ba663
Alex Libório Caranha <alex.caranha@gmail.com>
2021-06-12 15:11:30 -0300

    some visual changes.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
-from kivy.lang import Builder
+
+from kivy.core.window import Window
+# from kivy.lang import Builder
 
 from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
@@ -7,69 +9,17 @@ from kivy.properties import ObjectProperty
 
 from kivymd.app import MDApp
 
-KV = '''
-<ContentNavigationDrawer>:
-
-    ScrollView:
-        MDList:
-            OneLineListItem:
-                text: "Screen 1"
-                on_press:
-                    root.nav_drawer.set_state("close")
-                    root.screen_manager.current = "scr 1"
-
-            OneLineListItem:
-                text: "Screen 2"
-                on_press:
-                    root.nav_drawer.set_state("close")
-                    root.screen_manager.current = "scr 2"
-
-
-Screen:
-    MDToolbar:
-        id: toolbar
-        pos_hint: {"top": 1}
-        elevation: 10
-        title: "MDNavigationDrawer"
-        left_action_items: [["menu", lambda x: nav_drawer.set_state("open")]]
-
-    MDNavigationLayout:
-        x: toolbar.height
-
-        ScreenManager:
-            id: screen_manager
-
-            Screen:
-                name: "scr 1"
-
-                MDLabel:
-                    text: "Screen 1"
-                    halign: "center"
-
-            Screen:
-                name: "scr 2"
-
-                MDLabel:
-                    text: "Screen 2"
-                    halign: "center"
-
-        MDNavigationDrawer:
-            id: nav_drawer
-
-            ContentNavigationDrawer:
-                screen_manager: screen_manager
-                nav_drawer: nav_drawer
-'''
-
 class ContentNavigationDrawer(BoxLayout):
     screen_manager = ScreenManager()
     nav_drawer = ObjectProperty()
 
 
-class MyLauncher(MDApp):
+class Main(MDApp):
+    def change_title_visibility(self, enabled):
+        Window.borderless = not enabled
+
     def build(self):
-        # Window.borderless = True
-        return Builder.load_string(KV)
+        self.change_title_visibility(False)
 
 
-MyLauncher().run()
+Main().run()
