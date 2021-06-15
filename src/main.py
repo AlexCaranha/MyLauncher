4a82229c7e737c71cf2c7e03ae5fa44192d6bbc2
Alex Libório Caranha <alex.caranha@gmail.com>
2021-06-15 11:11:32 -0300

    some advances.

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from os import name
 from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
@@ -5,16 +6,14 @@ from kivy.uix.screenmanager import ScreenManager
 
 from kivy.properties import ObjectProperty
 from kivymd.app import MDApp
+from view_util import add_list_items_in_scroll, get_component_by_id
+from kivymd.uix.list import OneLineListItem
 
 
-class ContentNavigationDrawer123(BoxLayout):
-    def build(self):
-        screen_manager = ScreenManager()
-        nav_drawer = ObjectProperty()
+class ContentNavigationDrawer(BoxLayout):
+    screen_manager = ScreenManager()
+    nav_drawer = ObjectProperty()
 
-        # self.add_widget(screen_manager)
-        # self.add_widget(nav_drawer)
-        return self
 
 class Main(MDApp):
     def change_title_visibility(self, enabled):
@@ -26,6 +25,36 @@ class Main(MDApp):
 
     def on_start(self):
         print("\non_start:")
+        self.setup()
+
+    def setup(self):
+        self.setup_left_scroll()
+
+    def setup_left_scroll(self):
+        scroll = get_component_by_id("scroll_view", self.root)
+        widgets = list()
+
+        item1 = OneLineListItem(text="Search", on_press=self.on_press_scroll)
+        item1.name="item1"
+        widgets.append(item1)
+
+        item2 = OneLineListItem(text="Configuration", on_press=self.on_press_scroll)
+        item2.name="item2"
+
+        widgets.append(item2)
+        add_list_items_in_scroll(widgets, scroll)
+
+    def on_press_scroll(self, sender):
+        nav_drawer = get_component_by_id("nav_drawer", self.root)
+        nav_drawer.set_state("close")
+
+        screen_manager = get_component_by_id("screen_manager", self.root)
+
+        if sender.name == "item1":
+            screen_manager.current = "scr 1"
+
+        if sender.name == "item2":
+            screen_manager.current = "scr 2"
 
     def on_stop(self):
         print("\non_stop:")
@@ -34,3 +63,5 @@ class Main(MDApp):
         print("key pressed.")
 
 Main().run()
+
+# Criar função para encontrar componente recursivamente pelo id ou nome.
--- src/view_util.py
+++ src/view_util.py
@@ -11,3 +11,41 @@ def create_scroll_list_items(list_items):
 
     scroll.add_widget(md_list)
     return scroll
+
+def add_list_items_in_scroll(list_items, scroll):
+    md_list = MDList()
+
+    for item in list_items:       
+        md_list.add_widget(item)
+
+    scroll.add_widget(md_list)
+
+def get_component_by_name(name, object):
+    if object == None or object.children == None:
+        return None
+
+    if hasattr(object, 'name') and name == object.name:
+        return object
+
+    for child in object.children:
+       found = get_component_by_name(name, child) 
+
+       if found != None:
+           return found
+
+    return None
+
+def get_component_by_id(id, object):
+    if object == None or object.children == None:
+        return None
+
+    if hasattr(object, 'ids') and id in object.ids:
+        return object.ids[id]
+
+    for child in object.children:
+       found = get_component_by_id(id, child) 
+
+       if found != None:
+           return found
+
+    return None
